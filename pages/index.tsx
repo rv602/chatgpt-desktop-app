3afbf203f499181a6ef1b67ba374da8562f93447
rv602 <rajagopalan602@gmail.com>
2022-12-05 21:09:49 +0530

    build done

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react"
+
+export default function Home() {
+
+  useEffect(() => {
+    window.location.href = 'https://chat.openai.com/chat'
+  })
+
+  return (
+    <>
+    </>
+  )
+}
